ae22ae5a2a7f88d8592f6f3475bd8ebe23e8e700
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-24 15:59:32 -0500

    Added a global getter and setter for health points to be called upon later

--- src/tamagotchi.java
+++ src/tamagotchi.java
@@ -4,47 +4,114 @@ import java.util.TimerTask;
 
 public class tamagotchi {
 
+    public int healthPoints;
+
+    public int getHealthPoints(){
+        return healthPoints;
+    }
+
+    public int setHealthPoints(int changeValue){
+        return healthPoints + changeValue;
+    }
+
     static class WakeUpTama extends TimerTask {
         public void run() {
+
             System.out.println("Your tamagotchi is awake! Yay!");
+
         }
     }
 
-    public static void starterStats (){
+    class DepleteHealth extends TimerTask {
+        public void run() {
+
+            System.out.println("-1 Health");
+
+            setHealthPoints(-1);
+
+        }
+    }
+
+    public static void starterStats() {
         // starts at 1/10 health
         // age: 0
     }
 
-//    TODO: Make it die
-    public static void death (){
-        // switch statment:
-            // if health = 0, death
-            // if health > =. life
-            // if age = 27, death
+    public static void death() {
+        // switch statement:
+        // if health = 0, death
+        // if health > =. life
+        // if age = 27, death
     }
 
-    public static void feedSnack (){
+    public int feedSnack(int hp) {
         // snack gives +1 health
+        return setHealthPoints(+1);
     }
 
-    public static void feedMeal (){
+    public int feedMeal(int hp) {
         // meal gives +5 health
+        return setHealthPoints(+5);
     }
 
-    public static void healthMeter (){
+    public void healthMeter() {
         // 0 - 10
-    }
+        // [**********] = 10 hp
+        // [*********-] = 9 hp
+        // [********--] = 8 hp
+        // [*******---] = 7 hp
+        // [******----] = 6 hp
+        // [*****-----] = 5 hp
+        // [****------] = 4 hp
+        // [***-------] = 3 hp
+        // [**--------] = 2 hp
+        // [*---------] = 1 hp
+        // [-----DEAD!-----] = 0 hp
 
-    static class DepleteHealth extends TimerTask {
-        public void run() {
-            System.out.println("-1 Health");
+
+        switch (getHealthPoints()) { //based on the numerical grade value --> do the corresponding task
+            case 10:
+                System.out.println("[**********]");
+                break;
+            case 9:
+                System.out.println("[*********-]");
+                break;
+            case 8:
+                System.out.println("[********--]");
+                break;
+            case 7:
+                System.out.println("[*******---]");
+                break;
+            case 6:
+                System.out.println("[******----]");
+                break;
+            case 5:
+                System.out.println("[*****-----]");
+                break;
+            case 4:
+                System.out.println("[****------]");
+                break;
+            case 3:
+                System.out.println("[***-------]");
+                break;
+            case 2:
+                System.out.println("[**--------]");
+                break;
+            case 1:
+                System.out.println("[*---------]");
+                break;
+            default:
+                System.out.println("[-----DEAD!-----]");
+                break;
+
         }
+
+
     }
 
 
+    public static void main(String[] args) {
 
-
-    public static void main (String [] args){
         Scanner listen = new Scanner(System.in);
 //        if user says "wakeup", call setTimer and then starterStats
 //        if user says "feed snack", +1 health
@@ -53,5 +120,8 @@ public class tamagotchi {
         Timer timer = new Timer();
         timer.schedule(new WakeUpTama(), 5000);
         timer.schedule(new DepleteHealth(), 5000, 5000);
+
+
+
     }
 }
