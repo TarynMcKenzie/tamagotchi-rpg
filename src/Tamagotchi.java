158e6fe971248537569d1202de5297d0f70cf93b
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-08 14:33:23 -0500

    Added more pseudo coding to get the tamagotchi character structure established

--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -3,19 +3,26 @@ public class Tamagotchi implements TamagotchiHabits{
     // class that creates the tamagotchi objects while implementing the TamagotchiHabits interface
 
     // =============== INITIALIZATION ===============
+    protected String userDefinedName;
+
 
 
     // =============== CONSTRUCTOR METHOD ===============
-    public Tamagotchi(){
+    public Tamagotchi(String tamagotchiName){
 
         // How the tamagotchi will be constructed
+        this.userDefinedName = tamagotchiName;
 
     }
 
 
     // =============== METHOD IMPLEMENTATION ===============
+
+    // ------------- Level Progression -------------
     @Override
     public void incubation() {
+        // Goal: egg --> child
+
         // Will the user name their character at this point? Or after it hatches?
         // When will the egg hatch?
         // Will the user be required to care for the egg? Or just wait?
@@ -23,34 +30,9 @@ public class Tamagotchi implements TamagotchiHabits{
     }
 
     @Override
-    public void bored() {
-        // How long will it take for the tama to get bored?
-    }
-
-    @Override
-    public void eat() {
-        // How much health points will it receive for snacks?
-        // What snacks are available?
-        // How much health points will it receive for meals?
-        // What meals are available?
-    }
-
-    @Override
-    public void poop() {
-        // Will it got both number 1 and 2?
-        // How often will it go?
-        // At what point will it become a health risk?
-    }
-
-    @Override
-    public void sleep() {
-        // How much sleep does it require?
-        // How much awake time before it gets tired?
-        // At what point is sleep deprivation a health hazard?
-    }
-
-    @Override
     public void evolve() {
+        // Goal: child --> adult
+
         // How many levels are there?
         // When will the tama achieve the next level? How?
         // Will they die of old age?
@@ -60,6 +42,85 @@ public class Tamagotchi implements TamagotchiHabits{
 
     @Override
     public void die(){
+        // Goal: child --> die or adult --> die
+
         // When the tama dies, what will be displayed to the user?
     }
+
+
+
+
+    // ------------- Non-changeable Stats -------------
+    @Override
+    public void age(){
+        // Goal: days from egg --> present state
+
+        // Will the tama age according to our own age progressions? Faster? Slower?
+    }
+
+    @Override
+    public void gender(){
+        // Goal: // male or female
+
+        // How will the gender be determined?
+        // Can the user choose the gender?
+    }
+
+
+
+    // ------------- Health Meters -------------
+    @Override
+    public void happy(){
+        //Goal: heart health points (max = 4 hearts)
+
+        // What will determines the tama happiness?
+        // How fast will their happiness deplete?
+    }
+
+    @Override
+    public void hungry(){
+        //Goal: hunger health points (max = 4 hearts)
+
+        // What will determine the tama hunger?
+        // How fast will their hunger deplete?
+    }
+
+
+
+    // ------------- User Affected Stats -------------
+    @Override
+    public void bored() {
+        //Goal: (after x hours) if played with --> +2 , else --> -2
+
+        // How long will it take for the tama to get bored?
+    }
+
+    @Override
+    public void eat() {
+        //Goal: (after x hours) if fed --> +2 , else --> -2  ** snack +1 or -1 **
+
+        // How much health points will it receive for snacks?
+        // What snacks are available?
+        // How much health points will it receive for meals?
+        // What meals are available?
+    }
+
+    @Override
+    public void poop() {
+        //Goal: (after x hours --> poop) , (after x hours) if not cleaned up by user -2, else +2
+
+        // Will it got both number 1 and 2?
+        // How often will it go?
+        // At what point will it become a health risk?
+    }
+
+    @Override
+    public void sleep() {
+        // Goal: (after x hours) if put to sleep +3, else -3
+
+        // How much sleep does it require?
+        // How much awake time before it gets tired?
+        // At what point is sleep deprivation a health hazard?
+    }
+
 }
--- src/TamagotchiHabits.java
+++ src/TamagotchiHabits.java
@@ -5,18 +5,26 @@ public interface TamagotchiHabits {
      When a tamagotchi is created they will have an incubation period (egg status)
      Following the incubation period the egg will hatch. Out pops your brand new tamagotchi character chosen at random.
      Your tamagotchi will need to be played with, fed, use the bathroom, put to sleep.
-     If taken care of well, the tamagotchi will evolve to the next level (3 levels: infant, teen, adult)
+     If taken care of well, the tamagotchi will evolve to the next level (4 levels: egg, child, teen, adult)
      */
 
-    void incubation();
 
-    void bored();
-    void eat();
-    void poop();
-    void sleep();
+    // ------------- Level Progression -------------
+    void incubation(); // egg --> child
+    void evolve(); // child --> adult
+    void die(); // child --> die  or  adult --> die
 
-    void evolve();
+    // ------------- Non-changeable Stats -------------
+    void age(); // days from egg --> present state
+    void gender(); // male or female
 
-    void die();
+    // ------------- Health Meters -------------
+    void happy(); // heart health points (max = 4 hearts)
+    void hungry(); // hunger health points (max = 4 hearts)
 
+    // ------------- User Affected Stats -------------
+    void bored(); // (after x hours) if played with --> +2 , else --> -2
+    void eat(); // (after x hours) if fed --> +2 , else --> -2  ** snack +1 or -1 **
+    void poop(); // (after x hours --> poop) , (after x hours) if not cleaned up by user -2, else +2
+    void sleep(); // (after x hours) if put to sleep +3, else -3
 }
